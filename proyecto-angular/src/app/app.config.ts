8bd3c83e0cb5ea89a490666a7c0356c503df4b54
Danunu04 <dperelmuter25@gmail.com>
2025-11-10 15:34:58 -0300

    Creacion del entorno

---
+++ proyecto-angular/src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG({})
+  ]
+};
