4cda0f0229f5242bef42a975afcabc34d579dafd
Danunu04 <dperelmuter25@gmail.com>
2025-11-11 14:15:30 -0300

    Propuesta de diseño

--- proyecto-angular/src/app/app.component.ts
+++ proyecto-angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductosMainComponent } from "./features/productos/pages/productosMain/productos-main/productos-main.component";
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatSidenavModule} from '@angular/material/sidenav'; 
 
 @Component({
   selector: 'app-root',
-  imports: [ProductosMainComponent, RouterOutlet],
+  imports: [ProductosMainComponent, RouterOutlet, MatIconModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
